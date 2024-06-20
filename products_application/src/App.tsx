38bf57f4880dbd03d4db2a5d59be27df23abd109
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-20 14:08:19 -0500

    update: routes configuration

--- products_application/src/App.tsx
+++ products_application/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes , Route} from 'react-router-dom'
+import Home from './components/pages/Home/Home'
+import About from './components/pages/About/About'
+import Error from './components/pages/Error/Error'
+import Product from './components/pages/Products/Product'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path='/' element={ <Home></Home> }>
+          <Route path='about' element={ <About></About> }></Route>
+          <Route path='products' element={ <Product></Product> }></Route>
+          <Route path='*' element={ <Error></Error> }></Route>
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ products_application/src/components/pages/Products/Product.tsx
@@ -0,0 +1,5 @@
+function Product (){
+    return <h1>products</h1>;
+}
+
+export default Product;
